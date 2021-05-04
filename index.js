a9eacec86825549d124067b5e7e03bfc247224a7
gabrieleligetta <gabligetta@gmail.com>
2021-05-04 15:03:40 +0200

    tentative with token variable

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cron = require('node-cron')
 const express = require('express')
 const path = require('path')
 require('dotenv').config();
-const token = process.env.TELEGRAM_TOKEN;
+const token = process.env.BOT_TOKEN;
 const PORT = process.env.PORT || 5000
 express()
     .use(express.static(path.join(__dirname, 'public')))
