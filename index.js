5a3b5782ec08f27cd5ba215b3aff076dbc19e755
gabrieleligetta <gabligetta@gmail.com>
2021-05-04 12:52:22 +0200

    port added

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const telegraf = require('telegraf')
 const axios = require('axios')
 const randomItem = require('random-item')
 const faker = require('faker')
-const cron = require('node-cron');
+const cron = require('node-cron')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const bot = new telegraf(***REMOVED***)
 
