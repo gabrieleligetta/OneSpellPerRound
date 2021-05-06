965b0b555c76ba4852a6cc300e618622d03532c2
gabrieleligetta <gabligetta@gmail.com>
2021-05-06 15:01:52 +0200

    refactoring del codice

---
+++ persona.js
@@ -0,0 +1,42 @@
+const formatter = require("./formatter");
+const factory = require("./factory");
+const scoresClass = require("./scores");
+
+
+module.exports = {
+    getStandardChar:async function(msg,level,mode) {
+        let name = factory.getName(msg)
+        let classe = await factory.getClass(level) //cachato
+        let race = await factory.getRace() //cachato
+        let subrace = await factory.getSubrace(race) //cachato
+        let traits = await factory.getTraits(race,subrace,level)
+        let scores //cachato
+        switch (mode) {
+            case 'standard':
+                scores = await scoresClass.getStandardScores(race,subrace)
+                break
+            case 'rolled':
+                scores = await scoresClass.getRolledScores(race,subrace)
+                break
+            case 'best':
+                scores = await scoresClass.getBestScores(race,subrace,classe)
+                break
+            default:
+                scores = await scoresClass.getStandardScores(race,subrace)
+        }
+        let background =  factory.getBackground() //locale
+        let proficiencies = await factory.getProficiencies(background.char_background,classe,race) //cachato
+        let character = {
+            name : name,
+            level: level,
+            classe : classe,
+            race : race,
+            traits : traits,
+            subrace : subrace,
+            scores : scores,
+            background : background,
+            proficiencies : proficiencies
+        }
+        return formatter.getFormattedReply(character);
+    }
+};
---
+++ utils.js
@@ -0,0 +1,32 @@
+const faker = require('faker')
+
+module.exports = {
+
+    getRandomName:function() {
+    return faker.name.findName();
+    },
+    abstractDice:function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+    },
+    makeUpperCaseAfterCommas:function(str) {
+    return str.replace(/,\s*([a-z])/g, function(d,e) { return ", "+e.toUpperCase() });
+    },
+    replyEscaper:function(reply) {
+    reply = reply.split("-").join("\-")
+    reply = reply.split(".").join("\.")
+    reply = reply.split("(").join("\(")
+    reply = reply.split(")").join("\)")
+    return reply
+    },
+    removeFromArray: function(array,value) {
+    let index = array.indexOf(value)
+    return array.slice(index,1)
+},
+    removeSmallest:function(arr) {
+    const smallest = Math.min(...arr);
+    const index = arr.indexOf(smallest);
+
+    return arr.filter((_, i) => i !== index);
+    }
+
+};
