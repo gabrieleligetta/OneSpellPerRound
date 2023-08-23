d6e0da8074b3928807f2c878d4c8e8ec3338975b
gabriele ligetta <g.ligetta@notomia.com>
2023-08-24 00:37:42 +0200

    added model choice to Prompt

--- utils.js
+++ utils.js
@@ -1,93 +1,115 @@
-const faker = require('faker')
+const faker = require("faker");
 
 const Prompts = {
-    MartaLaPapera: "MartaLaPapera",
-    BattuteDnD: "BattuteDnD",
-    EpisodePromptValues: "EpisodePromptValues",
-}
+  MartaLaPapera: "MartaLaPapera",
+  BattuteDnD: "BattuteDnD",
+  EpisodePromptValues: "EpisodePromptValues",
+};
 
 function extractStringBetweenCharacters(inputString, char1, char2) {
-    const regex = new RegExp(`${char1}.*?${char2}`);
-    const result = inputString.match(regex);
+  const regex = new RegExp(`${char1}.*?${char2}`);
+  const result = inputString.match(regex);
 
-    return result ? result[0] : null;
+  return result ? result[0] : null;
 }
 
 function getRandomElementsFromArray(arr, number) {
-    if (number< 1||typeof number !== 'number') {
-        number = 2
+  if (number < 1 || typeof number !== "number") {
+    number = 2;
+  }
+  if (!Array.isArray(arr)) {
+    throw new Error(
+      "Invalid input. The first argument should be an array, and the second argument should be a positive number."
+    );
+  }
+
+  if (arr.length <= 2) {
+    throw new Error("The input array should have at least two elements.");
+  }
+
+  const selectedElements = [];
+
+  while (selectedElements.length < number) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const randomElement = arr[randomIndex];
+
+    if (!selectedElements.includes(randomElement)) {
+      selectedElements.push(randomElement);
+      arr = arr.filter((el) => el !== randomElement);
     }
-    if (!Array.isArray(arr)) {
-        throw new Error('Invalid input. The first argument should be an array, and the second argument should be a positive number.');
-    }
+  }
 
-    if (arr.length <= 2) {
-        throw new Error('The input array should have at least two elements.');
-    }
-
-    const selectedElements = [];
-
-    while (selectedElements.length < number) {
-        const randomIndex = Math.floor(Math.random() * arr.length);
-        const randomElement = arr[randomIndex];
-
-        if (!selectedElements.includes(randomElement)) {
-            selectedElements.push(randomElement);
-            arr = arr.filter(el => el !== randomElement)
-        }
-    }
-
-    return selectedElements;
+  return selectedElements;
 }
 
 const removeCharExceptFirstAndLast = (inputString, character) => {
-    const firstIndex = inputString.indexOf(character);
-    const lastIndex = inputString.lastIndexOf(character);
+  const firstIndex = inputString.indexOf(character);
+  const lastIndex = inputString.lastIndexOf(character);
 
-    if (firstIndex === -1 || firstIndex === lastIndex) {
-        return inputString;
-    }
+  if (firstIndex === -1 || firstIndex === lastIndex) {
+    return inputString;
+  }
 
-    const charactersToRemove = inputString.slice(firstIndex + 1, lastIndex).split(character).join('');
-    return inputString.slice(0, firstIndex + 1) + charactersToRemove + inputString.slice(lastIndex);
-}
+  const charactersToRemove = inputString
+    .slice(firstIndex + 1, lastIndex)
+    .split(character)
+    .join("");
+  return (
+    inputString.slice(0, firstIndex + 1) +
+    charactersToRemove +
+    inputString.slice(lastIndex)
+  );
+};
 
 const chunk = (str, size) =>
-    Array.from({ length: Math.ceil(str.length / size) }, (v, i) =>
-        str.slice(i * size, i * size + size)
-    );
+  Array.from({ length: Math.ceil(str.length / size) }, (v, i) =>
+    str.slice(i * size, i * size + size)
+  );
 
 module.exports = {
-    Prompts,
-    extractStringBetweenCharacters,
-    chunk,
-    getRandomElementsFromArray,
-    removeCharExceptFirstAndLast,
-    getRandomName:function() {
+  Prompts,
+  extractStringBetweenCharacters,
+  chunk,
+  getRandomElementsFromArray,
+  removeCharExceptFirstAndLast,
+  getRandomName: function () {
     return faker.name.findName();
-    },
-    abstractDice:function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
-    },
-    makeUpperCaseAfterCommas:function(str) {
-    return str.replace(/,\s*([a-z])/g, function(d,e) { return ", "+e.toUpperCase() });
-    },
-    replyEscaper:function(reply) {
-    reply = reply.split("-").join("\-")
-    reply = reply.split(".").join("\.")
-    reply = reply.split("(").join("\(")
-    reply = reply.split(")").join("\)")
-    return reply
-    },
-    removeFromArray: function(array,value) {
-    let index = array.indexOf(value)
-    return array.splice(index,1)
-},
-    removeSmallest:function(arr) {
+  },
+  abstractDice: function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  makeUpperCaseAfterCommas: function (str) {
+    return str.replace(/,\s*([a-z])/g, function (d, e) {
+      return ", " + e.toUpperCase();
+    });
+  },
+  replyEscaper: function (reply) {
+    reply = reply.split("-").join("-");
+    reply = reply.split(".").join(".");
+    reply = reply.split("(").join("(");
+    reply = reply.split(")").join(")");
+    return reply;
+  },
+  removeFromArray: function (array, value) {
+    let index = array.indexOf(value);
+    return array.splice(index, 1);
+  },
+  removeSmallest: function (arr) {
     const smallest = Math.min(...arr);
     const index = arr.indexOf(smallest);
 
     return arr.filter((_, i) => i !== index);
+  },
+  makeid: function (length) {
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
     }
-
+    return result;
+  },
 };
