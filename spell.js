9f1be56b69193bf4998bdaf41ac837d9ddcd56ea
gabriele ligetta <g.ligetta@notomia.com>
2023-09-04 19:22:40 +0200

    creazione Pg funzionante omg

--- spell.js
+++ spell.js
@@ -1,11 +1,14 @@
 import randomItem from "random-item";
+import { getSpells } from "./cache.js";
+import { getSpellByIndex } from "./factory.js";
+import { getFormattedSpell } from "./formatter.js";
 
 export const getSpell = async function (index) {
   switch (index) {
     case "random":
       let spellsArray = await getSpells();
-      let index = randomItem(spellsArray.data.results).index;
-      let spell = await factory.getSpellByIndex(index);
-      return (spell = formatter.getFormattedSpell(spell));
+      let index = randomItem(spellsArray.data.results);
+      let spell = await getSpellByIndex(index?.index);
+      return (spell = getFormattedSpell(spell));
   }
 };
